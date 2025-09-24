d49dc2681f968c4a530bde6f918ce1b17b0fed32
Raphael <rewoma19@gmail.com>
2025-09-23 22:37:09 -0600

    Generate a combination to open their safe.

--- Higher Order Functions/secret_agent_double/SecretAgent.swift
+++ Higher Order Functions/secret_agent_double/SecretAgent.swift
@@ -20,3 +20,12 @@ func protectSecret(_ secret: String, withPassword password: String) -> (String) 
     return secretFunction
 }
 
+func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
+    let firstCodeNum: Int = f(room)
+    let secondCodeNum: Int = f(firstCodeNum)
+    let thirdCodeNum: Int = f(secondCodeNum)
+    
+    let finalCombination = (firstCodeNum, secondCodeNum, thirdCodeNum)
+    return finalCombination
+}
+
